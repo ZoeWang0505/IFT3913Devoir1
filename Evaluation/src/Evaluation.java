1414d9241e056b54d88e83e2fde3af43835aaa11
zoe.wang0505 <zoe.wang0505@gmail.com>
2022-02-08 00:03:30 -0500

    implement counting of commtaire line from one class

--- Evaluation/src/Evaluation.java
+++ Evaluation/src/Evaluation.java
@@ -18,17 +18,38 @@ import java.util.Scanner;
 public class Evaluation {
    static String cHeaderlist [] = {"chemin", "class", "classe_LOC", "classe_CLOC", "classe_DC"};
    String pHeaderlist [] = {"chemin", "paquet", "paquet_LOC", "paquet_CLOC", "paquet_DC"};
+   static enum commentType {SINGLE_LINE, MULTICOMM_BEGIN, NON_COMM, MULTICOMM_END, IN_MULTCOMM};
    
-    static Boolean containCommtaire(String line){
-
-        String pattern = "\\/\\*(\\*(?!\\/)|[^*])*\\*\\/|\\/\\/";
-        Pattern r = Pattern.compile(pattern);
-        Matcher m = r.matcher(line);
-        if(m.find()){
-            return true;
+    static commentType containCommtaire(String line, Boolean inMultiCommtaire){
+        if(!inMultiCommtaire){
+            //Match if in single line comments such as \\ and /** */
+            String pattern = "\\/\\*(\\*(?!\\/)|[^*])*\\*\\/|\\/\\/";
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(line);
+            if(m.find()){
+                return commentType.SINGLE_LINE;
+            } else {
+                //Match if starting with /*
+                pattern = "\\/\\*";
+                r = Pattern.compile(pattern);
+                m = r.matcher(line); 
+                if(m.find()){
+                    return commentType.MULTICOMM_BEGIN;
+                }
+            }
+            return commentType.NON_COMM;
+        } else{
+            String pattern = "(\\*(?!\\/)|[^*])*\\*\\/";//Match if end with */
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(line);
+            if(m.find()){
+                return commentType.MULTICOMM_END;
+            }
+            return commentType.IN_MULTCOMM;
         }
-        return false;
+
     }
+
 
     static void parsingClass(File file,JSONObject eva_class){
         //Creating Scanner instance to read File in Java
@@ -38,14 +59,32 @@ public class Evaluation {
             //Reading each line of the file using Scanner class
             int lineNumber = 0;
             int commtaireline = 0;
+            boolean inMultiCommtaire = false;
             while(scan.hasNextLine()){
                 String line = scan.nextLine();
                 if(line.compareTo("") == 0)
                    continue;
-                if(containCommtaire(line)){
-                    commtaireline ++;
-                }
+                
                 lineNumber++;
+                commentType comType = containCommtaire(line, inMultiCommtaire);
+                switch (comType){
+                    case SINGLE_LINE:
+                        commtaireline ++;
+                        break;
+                    case MULTICOMM_BEGIN:
+                        commtaireline ++;
+                        inMultiCommtaire = true;
+                        break; 
+                    case MULTICOMM_END:
+                        inMultiCommtaire = false;
+                        commtaireline ++;
+                        break; 
+                    case IN_MULTCOMM:
+                        commtaireline ++;
+                        break; 
+                    default:
+                        break;
+                }
             }
             eva_class.put(cHeaderlist[3], lineNumber);//classe_LOC
             eva_class.put(cHeaderlist[4], commtaireline); //classe_CLOC    
